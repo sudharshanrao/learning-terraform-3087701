be4951c1b86f78160425d16176549688b8e85799
sudharshanrao <sudharshanrao@gmail.com>
2022-12-30 14:22:03 -0800

    fixed arns

--- main.tf
+++ main.tf
@@ -40,12 +40,11 @@ module "asg" {
   max_size                  = 2
 
   vpc_zone_identifier       = module.blog_vpc.public_subnets
-  target_groups_arns        = module.blog_alb.target_groups_arns
+  target_group_arns         = module.blog_alb.target_group_arns
   security_groups           = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
   instance_type      = var.instance_type
-
 }
 
 
