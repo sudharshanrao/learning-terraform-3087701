ec63050b078c82acd52799a7f8352277a4df2f12
sudharshanrao <sudharshanrao@gmail.com>
2022-12-30 12:50:43 -0800

    fixed vpc_id

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -47,7 +48,7 @@ module "blog_sg" {
   version = "4.13.0"
   name    = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
