b4da436b3f5edae2aede6b0060f73e6fe3c5073b
sudharshanrao <sudharshanrao@gmail.com>
2022-12-30 12:43:22 -0800

    referenced blog

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.dev.ami
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.dev.arn
+  value = aws_instance.blog.arn
 }
