a2b210509b18c6f0ef47333b532d5f3c2a3cc2b1
sudharshanrao <sudharshanrao@gmail.com>
2022-12-30 12:20:26 -0800

    fixed name of security group

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "web" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name    = "blog_new"
+  name    = "blog"
 
   vpc_id = module.vpc.public_subnets[0]
 
