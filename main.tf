a67eabdf92f2e221991cd9480358d6fc42f2ab46
sudharshanrao <sudharshanrao@gmail.com>
2022-12-30 12:03:19 -0800

    removed quotes for variable

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "blog_sg" {
   version = "4.13.0"
   name    = "blog_new"
 
-  vpc_id = "data.aws_vpc.default.id"
+  vpc_id = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
